2234f02638e7feecff1b48648a282ac88da66846
Badley Teddy <bdlteddy1020@gmail.com>
2025-08-16 16:00:12 +1000

    Commit delete of unused files

--- backend/routes/supportRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ctrl = require('../controllers/supportController');
-
-router.post('/', ctrl.createTicket);
-router.get('/', ctrl.getTickets);
-router.put('/:id', ctrl.updateTicket);
-
-module.exports = router;
